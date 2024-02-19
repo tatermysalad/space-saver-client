370db35fbe3ca8e1dd1bbdc858215fcd4e1fee92
Ed Dougherty <edd@topdesk.com>
2024-02-19 11:33:25 +1100

    fix: add vercel deploy file

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), eslintPlugin()],
   test: {
     globals: true,
